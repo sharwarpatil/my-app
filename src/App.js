d53a548e3dfff7a76828ca1619897efa941938f2
Sharwar <patilsharwar@gmail.com>
2024-07-19 13:13:31 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Login from "./pages/Login";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+export default Home;
