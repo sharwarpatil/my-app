d7d8674455c72787abc942208e9ae256015c68a1
Harshavardhan Reddy <57401710+theh4cker1769@users.noreply.github.com>
2024-07-22 10:59:35 +0530

    Update DataTable.jsx

--- src/pages/Home/DataTable.jsx
+++ src/pages/Home/DataTable.jsx
@@ -1,1 +1,24 @@
+import { Box } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
+const DataTable = ({ filteredRows, columns }) => {
+    return (
+        <Box sx={{ width: '100%' }}>
+            <DataGrid
+                rows={filteredRows}
+                columns={columns}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 5,
+                        },
+                    },
+                }}
+                pageSizeOptions={[5]}
+                disableRowSelectionOnClick
+            />
+        </Box>
+    )
+}
+
+export default DataTable
